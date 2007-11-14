9b345c57b53cad1ce02d2a85b000ffa87baa5647
fenixk19 <fenixk19@3f8bd890-ef56-11de-a89a-43a960051dd3>
2007-11-14 21:52:41 +0000

    New test for 4 bit mode will be written in future. _nosdlgraph test removed.

---
+++ examples/test_trafficlight.pp
@@ -0,0 +1,14 @@
+Uses SdlGraph, SdlGraph_Crt;
+
+Const Tab=20;
+
+Var W,H,GM, GD:Integer;
+Begin
+  GD:=D4bit;
+  GM:=m640x480;
+  InitGraph(GD, GM);
+  W:=GetMaxX;
+  H:=GetMaxY;
+  //We don't have enough functions, to write this example
+  //Rectangle(
+End.
