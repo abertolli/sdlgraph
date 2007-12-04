ba827cf5d0554d44185da048e17200f31efeaaf5
fenixk19 <fenixk19@3f8bd890-ef56-11de-a89a-43a960051dd3>
2007-12-04 20:05:43 +0000

    Flood fill fixes. All standart patterns added.

--- examples/test_trafficlight.pp
+++ examples/test_trafficlight.pp
@@ -4,16 +4,15 @@ Const Tab=20;
 
 Var W,H,GM, GD:Integer;
 Begin
-  SDLgraph_SetWindowed(true);
   GD:=D32bit;
   GM:=m320x256;
   InitGraph(GD, GM);
   W:=GetMaxX;
   H:=GetMaxY;
-  //We don't have enough functions, to write this example
+
   Rectangle(W div 2 -Tab, H div 2 -3*Tab, W div 2 + Tab, H div 2 + 3*Tab);
   SetColor(Red);
-  SetFillStyle(LineFill, Red);
+  SetFillStyle(CloseDotFill, Red);
   Circle(W div 2, H div 2 - 2*Tab, Round(Tab*0.9));
   FloodFill(W div 2, H div 2 - 2*Tab, Red);
 
